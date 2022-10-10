1980226f097f8f8ef6bc2258c7fb4c424dec2f91
ifeanyi305 <105528809+ifeanyi305@users.noreply.github.com>
2022-10-10 16:33:26 +0100

    Linters error checked and javascript modified

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-let hambuger = document.querySelector(" .hambuger")
-hambuger.addEventListener("click",function(){
-    hambuger.classList.toggle("active")
-})
+const hambuger = document.querySelector(' .hambuger');
+hambuger.addEventListener('click', () => {
+  hambuger.classList.toggle('active');
+});
+const anchor = document.querySelectorAll('.nav-item');
+for (let i = 0; i < anchor.length; i += 1) {
+  anchor[i].addEventListener('click', () => {
+    hambuger.classList.remove('active');
+  });
+}
